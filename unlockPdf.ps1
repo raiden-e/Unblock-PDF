4efabf09e1b26ad3cf8b553c2fa9e387be55f70c
Raiden Erdmann <raiden.erdmann@gmail.com>
2022-07-07 03:23:51 +0200

    Add Download and stuff

--- unlockPdf.ps1
+++ unlockPdf.ps1
@@ -1,14 +1,21 @@
 # ls .\bruh\ | % {.\qpdf-10.6.3\bin\qpdf.exe $_.FullName --decrypt "$($_.BaseName).unlocked.pdf"}
 [CmdletBinding()]
 param (
-	[Parameter()]
-	$Path
+    [Parameter(ValueFromPipeline, Mandatory)]
+    $Path
 )
 
-$qpdfHome = Get-Item $PSScriptRoot\qpdf*
+Remove-Item "qpdf.zip" -ErrorAction Ignore
+$qpdfHome = Get-Item "$PSScriptRoot\qpdf*"
 
-if(!$qpdfHome){
-	Invoke-WebRequest -UseBasicParsing
+if (!$qpdfHome) {
+    $Release = Invoke-WebRequest -UseBasicParsing "https://github.com/qpdf/qpdf/releases/latest"
+    $Release = $Release.Links.href | Where-Object { $_ -like "*mingw64*" }
+    Invoke-WebRequest -UseBasicParsing "https://github.com/$Release"  -OutFile "qpdf.zip" -ErrorAction Stop
+    Expand-Archive "qpdf.zip" -DestinationPath $PSScriptRoot -ErrorAction Stop
+    Remove-Item "qpdf.zip"
+
+    $qpdfHome = Get-Item "$PSScriptRoot\qpdf*"
 }
-	
+
 & "$qpdfHome\bin\qpdf.exe" $Path --decrypt "$path.unlocked.pdf"
